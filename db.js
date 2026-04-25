1a4df3c2d371fbc26cdfdcc4f0dc6a8b249639e3
Camps <773322626sadeq@gmail.com>
2026-04-25 19:01:18 +0300

    Fix connection string format in db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const postgres = require('postgres');
 
 const connectionString = process.env.DATABASE_URL || 
-  'postgresql://postgresql://postgres:[Sadeq@1982aa]@db.toymcbwhhfimqvdadwwl.supabase.co:5432/postgres';
+  'postgresql://postgres:[Sadeq@1982aa]@db.toymcbwhhfimqvdadwwl.supabase.co:5432/postgres';
 
 const sql = postgres(connectionString);
 
